1bfab1b2123357d4734476c25857b52243171324
Dipen Sagar <dipenrique@gmail.com>
2026-02-09 01:42:23 +0530

    frescopa code pull and merge

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,6 +33,22 @@ function buildHeroBlock(main) {
 }
 
 /**
+ * Move instrumentation attributes from a given element to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveInstrumentation(from, to) {
+  moveAttributes(
+    from,
+    to,
+    [...from.attributes]
+      .map(({ nodeName }) => nodeName)
+      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-')),
+  );
+}
+
+
+/**
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
